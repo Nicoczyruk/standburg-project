79a551fa3d7ba7fe563f566b3adde85c5a2a3022
gonzahag <gonzahag@gmail.com>
2025-04-16 12:42:33 -0300

    Scripts de la base de datos

---
+++ database/InsercionCategorias.sql
@@ -0,0 +1,8 @@
+INSERT INTO CATEGORIA(nombre, descripcion)
+VALUES ('Hamburguesas', 'Hamburguesas'),
+	   ('Bebidas', 'Bebidas'),
+	   ('Extras', 'Agregados'),
+	   ('Snacks', 'Acompañamiento'),
+	   ('Ensaladas', 'Ensaladas');
+
+select * from CATEGORIA
